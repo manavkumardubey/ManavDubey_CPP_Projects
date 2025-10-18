1b099049aa420bcf2d7f973225f17fb6e978cfdc
manavkumardubey <manavdubeyonly@gmail.com>
2025-10-18 11:43:28 +0530

    new file

---
+++ to_check_if_a_number_is_positive_negative_or_zero.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{ float n;
+    cin>>n;
+    if (n>0)
+    cout<<"Positive";
+    else if (n<0)
+    cout<<"Negative";
+    else cout<<"zero";
+    return 0;
+
+
+
+
+
+
+}
