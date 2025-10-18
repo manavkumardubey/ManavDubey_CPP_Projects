00d04690509184573a766850ec2e9d7dfcf57eb1
manavkumardubey <manavdubeyonly@gmail.com>
2025-10-18 12:15:24 +0530

    First Commit

---
+++ if_a_number_is_divisible_by_5_and_11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{ int n,m;
+cout<<"Enter your number"<<endl;
+cin>>n;
+if (n%5==0 && n%11==0)
+{cout<<"Yes the number is divisble by both 5 and 11";}
+else {cout<<"No the number is not divisble by both 5 and 11";}
+return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
