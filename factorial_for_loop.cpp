0c80b5f97a1d06cc69863b3e06ce55246629f787
manavkumardubey <manavdubeyonly@gmail.com>
2025-11-02 14:31:48 +0530

    modify: Factorial of n numbers by- for loop

---
+++ factorial_for_loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, fact=1;
+    cin>>n;
+    for(int x=1; x<=n; x++)
+    {fact = fact*x;}
+    cout<<fact;
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
